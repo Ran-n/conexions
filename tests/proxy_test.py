c6cd0de048abbae2e3c4ef862c991fa8c2d5d909
Ran# <ran.hash@proton.me>
2026-03-11 07:52:17 +0100

    [R] migrate tests to pytest and update project metadata
    - Convert proxy_test.py from unittest to pytest (fixtures, plain asserts)
    - Add real_ip module-scoped fixture and proxy function-scoped fixture
    - Remove redundant commented tests, keep and fix test_session_lifecycle
    - Update author email to ran.hash@proton.me in pyproject.toml

--- tests/proxy_test.py
+++ tests/proxy_test.py
@@ -3,363 +3,260 @@
 # ------------------------------------------------------------------------------
 #+ Authors:	Ran#
 #+ Created:	2022/02/12 19:50:16.098183
-#+ Revised:	2026/03/10 21:23:26.298045
+#+ Revised:	2026/03/11 07:50:20.231819
 # ------------------------------------------------------------------------------
-import unittest
+import pytest
 import requests
 
 from conexions.proxy import Proxy
 # ------------------------------------------------------------------------------
 
-class TestProxy(unittest.TestCase):
-    proxy_url: str = 'https://sslproxies.org'
-    ip_urls: list[str] = [
-            'https://ip.me',
-            'https://icanhazip.com'
-            ]
-
-    url: str = 'https://icanhazip.com'
-    real_ip: str = requests.get(url).text.rstrip()
-
-    """
-    def test_get_direct(self):
-        r = Proxy(verbose=False)
-
-        self.assertEqual(r.get_direct(self.url).text.rstrip(), self.real_ip)
-        self.assertEqual(r.get_direct_connection_count(), 1)
+PROXY_URL: str = 'https://sslproxies.org'
+IP_URLS: list[str] = [
+        'https://ip.me',
+        'https://icanhazip.com'
+        ]
+URL: str = 'https://icanhazip.com'
 
-        ip = r.get(self.url).text.rstrip()
-        self.assertNotEqual(ip, self.real_ip)
-        self.assertEqual(r.get_connection_count(), 1)
+# ------------------------------------------------------------------------------
 
-        self.assertEqual(r.get_direct(self.url).text.rstrip(), self.real_ip)
-        self.assertEqual(r.get_direct_connection_count(), 2)
+@pytest.fixture(scope='module')
+def real_ip() -> str:
+    return requests.get(URL).text.rstrip()
 
-    def test_get_proxy(self):
-        r = Proxy(verbose=False)
+@pytest.fixture
+def proxy() -> Proxy:
+    return Proxy()
 
-        ip = r.get(self.url).text.rstrip()
-        self.assertNotEqual(ip, self.real_ip)
-        #self.assertEqual(ip, r.get_proxy()['http'].split(':')[1][2:])
-        self.assertEqual(r.get_connection_count(), 1)
+# ------------------------------------------------------------------------------
 
-        ip2 = r.get(self.url).text.rstrip()
-        self.assertNotEqual(ip2, self.real_ip)
-        #self.assertEqual(ip2, r.get_proxy()['http'].split(':')[1][2:])
-        if ip != ip2:
-            self.assertEqual(r.get_connection_count(), 1)
-        else:
-            self.assertEqual(r.get_connection_count(), 2)
+# Getters
 
-    def test_get_direct_proxy(self):
-        r = Proxy(verbose=False)
+def test_get_url(proxy) -> None:
+    assert proxy.get_url() == PROXY_URL
 
-        self.assertEqual(r.get_direct(self.url).text.rstrip(), self.real_ip)
-        self.assertEqual(r.get_direct_connection_count(), 1)
-
-        ip = r.get(self.url).text.rstrip()
-        self.assertNotEqual(ip, self.real_ip)
-        #self.assertEqual(ip, r.get_proxy()['http'].split(':')[1][2:])
-        self.assertEqual(r.get_connection_count(), 1)
-
-    def test_session(self):
-        r = Proxy(verbose=False)
-
-        self.assertIsNone(r.get_session())
-        r.set_session()
-        self.assertIsNotNone(r.get_session())
-        r.set_session(reset=True)
-        self.assertIsNone(r.get_session())
-
-    def test_get_proxy_session(self):
-        r = Proxy(verbose=False)
-        r.set_session()
-
-        self.assertIsNotNone(r.get_session())
-
-        ip = r.get(self.url).text.rstrip()
-        self.assertNotEqual(ip, self.real_ip)
-        self.assertEqual(r.get_connection_count(), 1)
-
-        ip2 = r.get(self.url).text.rstrip()
-        self.assertNotEqual(ip2, self.real_ip)
-        if ip != ip2:
-            self.assertEqual(r.get_connection_count(), 1)
-        else:
-            self.assertEqual(r.get_connection_count(), 2)
-
-        ip3 = r.get(self.url).text.rstrip()
-        self.assertNotEqual(ip3, self.real_ip)
-
-        if ip3 == ip2:
-            if ip3 == ip2:
-                self.assertEqual(r.get_connection_count(), 3)
-            else:
-                self.assertEqual(r.get_connection_count(), 2)
-        else:
-            self.assertEqual(r.get_connection_count(), 1)
-
-        r.set_session(reset=True)
-        self.assertIsNone(r.get_session())
-    """
-
-    # Getters
-
-    def test_get_url(self) -> None:
-        p = Proxy()
-
-        self.assertEqual(p.get_url(), self.proxy_url)
-
-    def test_get_session(self) -> None:
-        p = Proxy()
-
-        self.assertIsNone(p.get_session())
-
-    def test_get_verbose(self) -> None:
-        self.assertFalse(Proxy().get_verbose())
-        self.assertTrue(Proxy(verbose= True).get_verbose())
-
-    def test_get_show_spinner(self) -> None:
-        self.assertFalse(Proxy().get_show_spinner())
-        self.assertTrue(Proxy(show_spinner= True).get_show_spinner())
-
-    def test_get_max_connections(self) -> None:
-        self.assertEqual(Proxy().get_max_connections(), 0)
-        self.assertEqual(Proxy(max_connections= 5).get_max_connections(), 5)
-
-    def test_get_total_connections(self) -> None:
-        p = Proxy()
-
-        self.assertEqual(p.get_total_connections(), 0)
-
-    def test_get_connection_count(self) -> None:
-        p = Proxy()
-
-        self.assertEqual(p.get_connection_count(), 0)
-
-    def test_get_direct_connection_count(self) -> None:
-        p = Proxy()
-
-        self.assertEqual(p.get_direct_connection_count(), 0)
-
-    def test_get_retries(self) -> None:
-        self.assertEqual(Proxy().get_retries(), 5)
-        self.assertEqual(Proxy(retries= 50).get_retries(), 50)
-
-    def test_get_timeout(self) -> None:
-        self.assertEqual(Proxy().get_timeout(), 30)
-        self.assertEqual(Proxy(timeout= 50).get_timeout(), 50)
-
-    def test_get_header(self) -> None:
-        p = Proxy()
-
-        h1 = p.get_header()
-        h2 = p.get_header(True)
-        self.assertIsNotNone(h1)
-        self.assertIsNotNone(h2)
-        self.assertEqual(h1, h2)
-
-        h3 = p.get_header(True)
-        self.assertIsNotNone(h3)
-        self.assertNotEqual(h1, h3)
-        self.assertNotEqual(h2, h3)
-
-        h4 = p.get_header()
-        self.assertIsNotNone(h4)
-        self.assertNotEqual(h1, h4)
-        self.assertNotEqual(h2, h4)
-        self.assertNotEqual(h3, h4)
-
-    def test_get_proxies(self) -> None:
-        self.assertIsNotNone(Proxy().get_proxies())
-
-    def test_get_proxy(self) -> None:
-        p = Proxy()
-
-        self.assertEqual(p.get_connection_count(), 0)
-        self.assertEqual(p.get_proxy(), p.get_proxy())
-
-    def test_priv_get_proxy(self) -> None:
-        p = Proxy()
-
-        self.assertEqual(p.get_connection_count(), 0)
-        self.assertEqual(p._Proxy__get_proxy(), p._Proxy__get_proxy())
-        self.assertEqual(p.get_connection_count(), 2)
-
-        p.set_max_connections(2)
-        self.assertEqual(p.get_connection_count(), 2)
-        self.assertEqual(p._Proxy__get_proxy(), p._Proxy__get_proxy())
-        self.assertEqual(p.get_connection_count(), 2)
-
-        p.set_max_connections(1)
-        self.assertNotEqual(p._Proxy__get_proxy(), p._Proxy__get_proxy())
-        self.assertNotEqual(p._Proxy__get_proxy(), p._Proxy__get_proxy())
-        self.assertEqual(p.get_connection_count(), 1)
-
-    def test_get_ip_urls(self) -> None:
-        p = Proxy()
-
-        self.assertEqual(p.get_ip_urls(), self.ip_urls)
-
-    # Getters #
-
-    # Setters
-
-    def test_priv_set_url(self) -> None:
-        p = Proxy()
-
-        new_url = 'a'
-
-        p._Proxy__set_url(new_url)
-        self.assertEqual(p.get_url(), new_url)
-
-    def test_set_session(self) -> None:
-        p = Proxy()
-
-        self.assertIsNone(p.get_session())
-
-        p.set_session()
-        self.assertIsNotNone(p.get_session())
-
-    def test_set_verbose(self) -> None:
-        p = Proxy()
-        self.assertFalse(p.get_verbose())
-
-        p.set_verbose(True)
-        self.assertTrue(p.get_verbose())
-
-    def test_set_show_spinner(self) -> None:
-        p = Proxy()
-        self.assertFalse(p.get_show_spinner())
-
-        p.set_show_spinner(True)
-        self.assertTrue(p.get_show_spinner())
-
-    def test_set_max_connections(self) -> None:
-        p = Proxy()
-        self.assertEqual(p.get_max_connections(), 0)
-
-        p.set_max_connections(11)
-        self.assertEqual(p.get_max_connections(), 11)
-
-    def test_priv_set_total_connections(self) -> None:
-        p = Proxy()
-        p._Proxy__set_total_connections(60)
-        self.assertEqual(p.get_total_connections(), 60)
-
-    def test_priv_set_connection_count(self) -> None:
-        p = Proxy()
-        p._Proxy__set_connection_count(6)
-        self.assertEqual(p.get_connection_count(), 6)
-
-    def test_priv_set_direct_connection_count(self) -> None:
-        p = Proxy()
-        p._Proxy__set_direct_connection_count(6)
-        self.assertEqual(p.get_direct_connection_count(), 6)
-
-    def test_set_retries(self) -> None:
-        p = Proxy()
-        self.assertEqual(p.get_retries(), 5)
-
-        p.set_retries(111)
-        self.assertEqual(p.get_retries(), 111)
-
-    def test_set_timeout(self) -> None:
-        p = Proxy()
-        self.assertEqual(p.get_timeout(), 30)
-
-        p.set_timeout(1411)
-        self.assertEqual(p.get_timeout(), 1411)
-
-    def test_set_header(self) -> None:
-        p = Proxy()
-
-        h1 = p.get_header()
-        h2 = p.get_header()
-        p.set_header()
-        h3 = p.get_header()
-        self.assertEqual(h1, h2)
-        self.assertNotEqual(h1, h3)
-        self.assertNotEqual(h2, h3)
-
-    def test_set_proxies(self) -> None:
-        p = Proxy()
-
-        l1 = p.get_proxies()
-        self.assertIsNotNone(l1)
-
-        p._Proxy__proxy_list = []
-        self.assertEqual(p.get_proxies(), [])
-
-        p.set_proxies()
-        l2 = p.get_proxies()
-        self.assertIsNotNone(l2)
-
-    def test_set_proxy(self) -> None:
-        p = Proxy()
-
-        self.assertIsNotNone(p.get_proxy())
-
-        proxy1 = p.get_proxy()
-        p.set_proxy()
-        self.assertNotEqual(proxy1, p.get_proxy())
-
-        while len(p.get_proxies()) > 0:
-            p.set_proxy()
-        self.assertEqual(len(p.get_proxies()), 0)
-
-        # trying to get a new proxy from an empty list triggers a re-scrape
-        p.set_proxy()
-        self.assertNotEqual(len(p.get_proxies()), 0)
-
-    # Setters #
-
-    def test_get_ip(self) -> None:
-        p = Proxy()
-
-        self.assertEqual(p.get_ip(), self.real_ip)
-
-    def test_get_direct(self) -> None:
-        p = Proxy()
-
-        ip_used = p.get_direct(self.url).text.rstrip()
-
-        self.assertIsNotNone(ip_used)
-        self.assertEqual(ip_used, self.real_ip)
-
-        self.assertTrue(p.get_direct_connection_count() >= 1)
-
-        p.set_max_connections(1)
-        ip_used2 = p.get_direct(self.url).text.rstrip()
-        self.assertEqual(ip_used, ip_used2)
-        self.assertTrue(p.get_connection_count() <= 1)
-
-        self.assertTrue(p.get_total_connections() == 2)
-
-    def test_get(self) -> None:
-        p = Proxy()
-
-        ip_used = p.get(self.url).text.rstrip()
-
-        self.assertEqual(p.get_proxy().https, 'yes')
-
-        self.assertIsNotNone(ip_used)
-        self.assertNotEqual(ip_used, self.real_ip)
-
-        len1 = len(p.get_proxies())
-        self.assertTrue(p.get_connection_count() >= 1)
-
-        p.set_max_connections(1)
-        ip_used2 = p.get(self.url).text.rstrip()
-
-        self.assertNotEqual(ip_used2, self.real_ip)
-
-        self.assertNotEqual(len1, len(p.get_proxies()))
-        self.assertNotEqual(ip_used, ip_used2)
-        self.assertTrue(p.get_connection_count() <= 1)
-
-        self.assertTrue(p.get_total_connections() >= 2)
-
-        p.get_direct(self.url).text.rstrip()
-        self.assertTrue(p.get_total_connections() >= 3)
+def test_get_session(proxy) -> None:
+    assert proxy.get_session() is None
+
+def test_get_verbose() -> None:
+    assert not Proxy().get_verbose()
+    assert Proxy(verbose=True).get_verbose()
+
+def test_get_show_spinner() -> None:
+    assert not Proxy().get_show_spinner()
+    assert Proxy(show_spinner=True).get_show_spinner()
+
+def test_get_max_connections() -> None:
+    assert Proxy().get_max_connections() == 0
+    assert Proxy(max_connections=5).get_max_connections() == 5
+
+def test_get_total_connections(proxy) -> None:
+    assert proxy.get_total_connections() == 0
+
+def test_get_connection_count(proxy) -> None:
+    assert proxy.get_connection_count() == 0
+
+def test_get_direct_connection_count(proxy) -> None:
+    assert proxy.get_direct_connection_count() == 0
+
+def test_get_retries() -> None:
+    assert Proxy().get_retries() == 5
+    assert Proxy(retries=50).get_retries() == 50
+
+def test_get_timeout() -> None:
+    assert Proxy().get_timeout() == 30
+    assert Proxy(timeout=50).get_timeout() == 50
+
+def test_get_header(proxy) -> None:
+    h1 = proxy.get_header()
+    h2 = proxy.get_header(True)
+    assert h1 is not None
+    assert h2 is not None
+    assert h1 == h2
+
+    h3 = proxy.get_header(True)
+    assert h3 is not None
+    assert h1 != h3
+    assert h2 != h3
+
+    h4 = proxy.get_header()
+    assert h4 is not None
+    assert h1 != h4
+    assert h2 != h4
+    assert h3 != h4
+
+def test_get_proxies(proxy) -> None:
+    assert proxy.get_proxies() is not None
+
+def test_get_proxy(proxy) -> None:
+    assert proxy.get_connection_count() == 0
+    assert proxy.get_proxy() == proxy.get_proxy()
+
+def test_priv_get_proxy(proxy) -> None:
+    assert proxy.get_connection_count() == 0
+    assert proxy._Proxy__get_proxy() == proxy._Proxy__get_proxy()
+    assert proxy.get_connection_count() == 2
+
+    proxy.set_max_connections(2)
+    assert proxy.get_connection_count() == 2
+    assert proxy._Proxy__get_proxy() == proxy._Proxy__get_proxy()
+    assert proxy.get_connection_count() == 2
+
+    proxy.set_max_connections(1)
+    assert proxy._Proxy__get_proxy() != proxy._Proxy__get_proxy()
+    assert proxy._Proxy__get_proxy() != proxy._Proxy__get_proxy()
+    assert proxy.get_connection_count() == 1
+
+def test_get_ip_urls(proxy) -> None:
+    assert proxy.get_ip_urls() == IP_URLS
+
+# Getters #
+
+# Setters
+
+def test_priv_set_url(proxy) -> None:
+    new_url = 'a'
+    proxy._Proxy__set_url(new_url)
+    assert proxy.get_url() == new_url
+
+def test_set_session(proxy) -> None:
+    assert proxy.get_session() is None
+    proxy.set_session()
+    assert proxy.get_session() is not None
+
+def test_set_verbose(proxy) -> None:
+    assert not proxy.get_verbose()
+    proxy.set_verbose(True)
+    assert proxy.get_verbose()
+
+def test_set_show_spinner(proxy) -> None:
+    assert not proxy.get_show_spinner()
+    proxy.set_show_spinner(True)
+    assert proxy.get_show_spinner()
+
+def test_set_max_connections(proxy) -> None:
+    assert proxy.get_max_connections() == 0
+    proxy.set_max_connections(11)
+    assert proxy.get_max_connections() == 11
+
+def test_priv_set_total_connections(proxy) -> None:
+    proxy._Proxy__set_total_connections(60)
+    assert proxy.get_total_connections() == 60
+
+def test_priv_set_connection_count(proxy) -> None:
+    proxy._Proxy__set_connection_count(6)
+    assert proxy.get_connection_count() == 6
+
+def test_priv_set_direct_connection_count(proxy) -> None:
+    proxy._Proxy__set_direct_connection_count(6)
+    assert proxy.get_direct_connection_count() == 6
+
+def test_set_retries(proxy) -> None:
+    assert proxy.get_retries() == 5
+    proxy.set_retries(111)
+    assert proxy.get_retries() == 111
+
+def test_set_timeout(proxy) -> None:
+    assert proxy.get_timeout() == 30
+    proxy.set_timeout(1411)
+    assert proxy.get_timeout() == 1411
+
+def test_set_header(proxy) -> None:
+    h1 = proxy.get_header()
+    h2 = proxy.get_header()
+    proxy.set_header()
+    h3 = proxy.get_header()
+    assert h1 == h2
+    assert h1 != h3
+    assert h2 != h3
+
+def test_set_proxies(proxy) -> None:
+    l1 = proxy.get_proxies()
+    assert l1 is not None
+
+    proxy._Proxy__proxy_list = []
+    assert proxy.get_proxies() == []
+
+    proxy.set_proxies()
+    assert proxy.get_proxies() is not None
+
+def test_set_proxy(proxy) -> None:
+    assert proxy.get_proxy() is not None
+
+    proxy1 = proxy.get_proxy()
+    proxy.set_proxy()
+    assert proxy1 != proxy.get_proxy()
+
+    while len(proxy.get_proxies()) > 0:
+        proxy.set_proxy()
+    assert len(proxy.get_proxies()) == 0
+
+    # trying to get a new proxy from an empty list triggers a re-scrape
+    proxy.set_proxy()
+    assert len(proxy.get_proxies()) != 0
+
+# Setters #
+
+def test_get_ip(proxy, real_ip) -> None:
+    assert proxy.get_ip() == real_ip
+
+def test_get_direct(proxy, real_ip) -> None:
+    ip_used = proxy.get_direct(URL).text.rstrip()
+    assert ip_used is not None
+    assert ip_used == real_ip
+    assert proxy.get_direct_connection_count() >= 1
+
+    proxy.set_max_connections(1)
+    ip_used2 = proxy.get_direct(URL).text.rstrip()
+    assert ip_used == ip_used2
+    assert proxy.get_connection_count() <= 1
+    assert proxy.get_total_connections() == 2
+
+def test_get(proxy, real_ip) -> None:
+    ip_used = proxy.get(URL).text.rstrip()
+    assert proxy.get_proxy().https == 'yes'
+    assert ip_used is not None
+    assert ip_used != real_ip
+
+    len1 = len(proxy.get_proxies())
+    assert proxy.get_connection_count() >= 1
+
+    proxy.set_max_connections(1)
+    ip_used2 = proxy.get(URL).text.rstrip()
+    assert ip_used2 != real_ip
+    assert len1 != len(proxy.get_proxies())
+    assert ip_used != ip_used2
+    assert proxy.get_connection_count() <= 1
+    assert proxy.get_total_connections() >= 2
+
+    proxy.get_direct(URL).text.rstrip()
+    assert proxy.get_total_connections() >= 3
+
+def test_session_lifecycle(real_ip) -> None:
+    p = Proxy()
+
+    assert p.get_session() is None
+    p.set_session()
+    assert p.get_session() is not None
+
+    ip = p.get(URL).text.rstrip()
+    assert ip != real_ip
+    assert p.get_connection_count() == 1
+
+    ip2 = p.get(URL).text.rstrip()
+    assert ip2 != real_ip
+    if ip != ip2:
+        assert p.get_connection_count() == 1
+    else:
+        assert p.get_connection_count() == 2
+
+    ip3 = p.get(URL).text.rstrip()
+    assert ip3 != real_ip
+    if ip3 == ip2:
+        assert p.get_connection_count() == 3
+    else:
+        assert p.get_connection_count() == 1
+
+    p.set_session(reset=True)
+    assert p.get_session() is None
 # ------------------------------------------------------------------------------
